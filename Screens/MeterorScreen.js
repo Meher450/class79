d22f5b7df9505fc6cce33adc42de7bb7e22bc205
Meher <82945138+Meher450@users.noreply.github.com>
2021-09-28 20:29:25 +0530

    Add files via upload

---
+++ Screens/MeterorScreen.js
@@ -0,0 +1,68 @@
+import React,{Component} from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+import axios from 'axios';
+
+export default class MeterorScreen extends Component{
+
+  constructor(props){
+    super(props);
+    this.state={
+      meteors:{},
+      
+    }
+  }
+  componentDidMount(){
+    this.getMeteors()
+  }
+
+
+  getMeteors=()=>{
+    axios
+    .get("https://api.nasa.gov/neo/rest/v1/feed?api_key=R4On5KryjRjhzfC9iJBo3UThj67K2kCV2Q168Jpe")
+    .then(response=>{
+      this.setState({meteors:response.data.near_earth_objects});
+    })
+    .catch(error=>{
+      alert(error.message)
+    })
+  }
+
+  render(){
+    if(Object.keys(this.state.location).length===0){
+      return(
+        <View style={{
+          flex:1,
+          justifyContent:'center',
+          alignItems:'center'
+        }}>
+        <Text>Loading..</Text>
+        </View>      
+      )
+    } else{
+       let meteorArray=Object.keys(this.state.meteors).map(meteor_date=>{
+         return this.state.meteors[meteor_date]
+       })
+       let meteors=[].concat.apply([],meteorArray)
+        meteors.forEach(function(element){
+          let diameter=(element.estimated_diameter.kilometers.estimated_diameter_min+
+          element.estimated_diameter.kilometers.estimated_diameter_max)/2
+        let threatScore=(diameter/element.close_approach_data[0].miss_distance.kilometers)*1000000000
+        element.threat_score=threatScore  
+        })
+    return(
+      <View style={styles.container}>
+      <Text>Meteror</Text>
+      </View>
+    )
+  }
+  }
+}
+
+const styles=StyleSheet.create({
+container:{
+  backgroundColor: "Black",
+  flex:1,
+}
+})
+
+//
